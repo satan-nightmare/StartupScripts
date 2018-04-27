03476717308a8bb1a55ba1a14bd2f28a3feee501
satan-nightmare <sumitsinghsakhwar@gmail.com>
2018-04-27 19:44:27 +0530

    Added initial files

---
+++ startupScripts.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get --assume-yes upgrade
+sudo apt-get --assume-yes install vlc gcc g++ vim youtube-dl sublime-text google-chrome-stable ubuntu-restricted-extras oracle-java9-installer oracle-java9-set-default gnome-tweak-tool
+
+javac -version
+
+# install tixati
+# alternate gnome
+# sudo apt install gnome-session
---
+++ startupScriptsNoProxy.sh
@@ -0,0 +1,13 @@
+# Sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+# Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+# Java
+sudo add-apt-repository ppa:webupd8team/java -y
+
+# Adapta theme
+sudo apt-add-repository ppa:tista/adapta -y
